2220123594ccf0ad5dee05ea42af9290026752de
Pandy Knight <andylpk.247@gmail.com>
2022-03-21 17:38:39 -0400

    Added browsers to Applitools config

--- applitools.config.js
+++ applitools.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   concurrency: 5,
-  batchName: "Visually Testing Storybook Components"
+  batchName: "Visually Testing Storybook Components",
+  browser: [
+    // Desktop
+    {width: 800, height: 600, name: 'chrome'},
+    {width: 700, height: 500, name: 'firefox'},
+    {width: 1600, height: 1200, name: 'ie11'},
+    {width: 1024, height: 768, name: 'edgechromium'},
+    {width: 800, height: 600, name: 'safari'},
+    // Mobile
+    {deviceName: 'iPhone X', screenOrientation: 'portrait'},
+    {deviceName: 'Pixel 2', screenOrientation: 'portrait'},
+    {deviceName: 'Galaxy S5', screenOrientation: 'portrait'},
+    {deviceName: 'Nexus 10', screenOrientation: 'portrait'},
+    {deviceName: 'iPad Pro', screenOrientation: 'landscape'},
+  ]
 }
